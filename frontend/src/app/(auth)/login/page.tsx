8cc5361e920857417761ce871afd077e546986ee
muhammadali304 <aliyousufpk304@gmail.com>
2026-01-13 22:48:57 +0500

    .

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
           fontSize: 'var(--font-size-sm)',
           color: 'var(--color-text-secondary)'
         }}>
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/register" style={{ color: 'var(--color-primary)', fontWeight: 500 }}>
             Sign up
           </Link>
